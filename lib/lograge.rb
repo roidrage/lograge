6a781f3fb486c92de9b9ee31247128423c0d6365
Mathias Meyer <meyer@paperplanes.de>
2012-03-10 12:52:10 +0100

    Extract slightly more generic unsubscribe.

--- lib/lograge.rb
+++ lib/lograge.rb
@@ -1,24 +1,25 @@
 require "lograge/version"
 require 'active_support/core_ext/module/attribute_accessors'
+require 'active_support/core_ext/string/inflections'
 
 module Lograge
   mattr_accessor :logger  
 
   def self.remove_existing_log_subscriptions
     %w(redirect_to process_action start_processing send_data write_fragment exist_fragment? send_file).each do |event|
-      ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_controller").each do |listener|
-        if listener.inspect =~ /delegate[^a-z]+ActionController/
-          ActiveSupport::Notifications.unsubscribe listener
-        end
-      end
-
+      unsubscribe_from_event(:action_controller, event)
     end
 
     %w{render_template render_partial render_collection}.each do |event|
-      ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_view").each do |listener|
-        if listener.inspect =~ /delegate[^a-z]+ActionView/
-          ActiveSupport::Notifications.unsubscribe listener
-        end
+      unsubscribe_from_event(:action_view, event) 
+    end
+  end
+
+  def self.unsubscribe_from_event(component, event)
+    delegate_type = component.to_s.classify
+    ActiveSupport::Notifications.notifier.listeners_for("#{event}.#{component}").each do |listener|
+      if listener.inspect =~ /delegate[^a-z]+#{delegate_type}/
+        ActiveSupport::Notifications.unsubscribe listener
       end
     end
   end
