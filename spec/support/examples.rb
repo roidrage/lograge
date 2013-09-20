7fc58f941be8bf62afa4f5f9aa3b3534c42ef8ad
Matt Button <matthew@sigswitch.com>
2013-09-20 16:52:46 +0200

    Refactor the key-value formatter spec to use shared examples, refs roidrage/lograge#47

---
+++ spec/support/examples.rb
@@ -0,0 +1,22 @@
+
+shared_examples_for "a key value formatter" do
+  let(:payload) do
+    {
+      custom: 'data',
+      status: 200,
+      method: 'GET',
+      path: '/',
+      controller: 'welcome',
+      action: 'index'
+    }
+  end
+
+  subject { described_class.new.call(payload) }
+
+  it { should include('method=GET') }
+  it { should include('path=/') }
+  it { should include('controller=welcome') }
+  it { should include('action=index') }
+  it { should include('status=200') }
+  it { should include('custom=data') }
+end
