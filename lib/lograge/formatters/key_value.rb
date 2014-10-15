caf7ffe40dbcfd9de5ea032f62787896751ade96
Peter Suschlik <ps@neopoly.de>
2014-10-15 17:05:43 +0200

    Remove needless data filter in key value formatter
    We don't need to filter data because we skip them anyway
    if the key does not exist.

--- lib/lograge/formatters/key_value.rb
+++ lib/lograge/formatters/key_value.rb
@@ -1,17 +1,10 @@
 module Lograge
   module Formatters
     class KeyValue
-      LOGRAGE_FIELDS = [
-        :method, :path, :format, :controller, :action, :status, :error,
-        :duration, :view, :db, :location
-      ]
-
       def call(data)
         fields = fields_to_display(data)
 
         event = fields.reduce([]) do |message, key|
-          next message unless data.key?(key)
-
           message << format(key, data[key])
           message
         end
@@ -19,7 +12,7 @@ module Lograge
       end
 
       def fields_to_display(data)
-        LOGRAGE_FIELDS + (data.keys - LOGRAGE_FIELDS)
+        data.keys
       end
 
       def format(key, value)
