ec5102e308ca6a21b961e973f39a5f67a05c49c6
Takashi Nakagawa <tak1240@gmail.com>
2015-06-29 23:33:33 +0900

    Add LTSV formatter and its spec

---
+++ lib/lograge/formatters/ltsv.rb
@@ -0,0 +1,43 @@
+module Lograge
+  module Formatters
+    class LTSV
+      def call(data)
+        fields = fields_to_display(data)
+
+        event = fields.map { |key| format(key, data[key]) }
+        event.join('\t')
+      end
+
+      def fields_to_display(data)
+        data.keys
+      end
+
+      def format(key, value)
+        if key == :error
+          # Exactly preserve the previous output
+          # Parsing this can be ambigious if the error messages contains
+          # a single quote
+          value = "'#{escape value}'"
+        else
+          # Ensure that we always have exactly two decimals
+          value = Kernel.format('%.2f', value) if value.is_a? Float
+        end
+
+        "#{key}:#{value}"
+      end
+
+      private
+
+      def escape(string)
+        value = string.is_a?(String) ? string.dup : string.to_s
+
+        value.gsub!('\\', '\\\\')
+        value.gsub!('\n', '\\n')
+        value.gsub!('\r', '\\r')
+        value.gsub!('\t', '\\t')
+
+        value
+      end
+    end
+  end
+end
