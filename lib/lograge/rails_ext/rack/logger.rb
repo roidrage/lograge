db501922d6df3e5e0648bf9f241d6d25c3e32fbc
Michael Bianco <iloveitaly@gmail.com>
2022-01-24 11:54:27 -0700

    Adding link to original logger

--- lib/lograge/rails_ext/rack/logger.rb
+++ lib/lograge/rails_ext/rack/logger.rb
@@ -17,6 +17,7 @@ module Rails
         env = args.last
         status, headers, body = @app.call(env)
         # needs to have same return type as the Rails builtins being overridden, see https://github.com/roidrage/lograge/pull/333
+        # https://github.com/rails/rails/blob/be9d34b9bcb448b265114ebc28bef1a5b5e4c272/railties/lib/rails/rack/logger.rb#L37
         [status, headers, ::Rack::BodyProxy.new(body)]
       ensure
         ActiveSupport::LogSubscriber.flush_all!
